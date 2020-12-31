33243cfdc8ea91ca4b0bfaa6ee73cd30169d0cb0
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2020-12-31 16:31:41 -0500

    Uploaded the basic game

---
+++ public/sketch.js
@@ -0,0 +1,147 @@
+var socket;
+var players;
+var platforms = [];
+var bullets = [];
+var deadPlayers = [];
+var gameStart = false;
+var userNameSubmitted = false;
+
+function setup(){
+    createCanvas(1200,600);
+    background(51);
+    frameRate(60);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('players', test);
+    socket.on("dead", respawn);
+
+    $(document).ready(function() {
+        //username modal start     
+        $('#myModal').appendTo('body').modal('show');
+        $('#myModal').on('hidden.bs.modal',function(){
+          if(!userNameSubmitted){
+            socket.emit('username',"");
+          }
+        });
+        $('#theButton').click(function() {
+          socket.emit('username',$('#userName').val());
+          userNameSubmitted = true;
+        });
+        //username modal end
+    
+        //focus on the textbox
+        $("#myModal").on('shown.bs.modal', function(){
+            $(this).find('#userName').focus();
+        });
+    
+        //when you press enter, closes the modal
+        $(document).keypress(function(e) {
+          var keycode = (event.keyCode ? event.keyCode : event.which);
+          if(keycode == '13'){
+            if($('#myModal').is(':visible')){
+              socket.emit('username',$('#userName').val());
+              userNameSubmitted = true;
+              $("#myModal").removeClass("in");
+              $(".modal-backdrop").remove();
+              $("#myModal").hide();
+            }  
+          }
+        });
+    });
+    
+}
+
+function draw() {
+    background(220);
+    if (gameStart){
+        socket.emit('update', 1)
+        socket.on('returnUpdate', update);
+    }
+    for (var i = 0; i < platforms.length; i++) {
+        if (platforms[i].speed === 0) {
+            fill(4, 207, 95);
+        } else {
+            fill(247, 191, 60);
+        }
+        rect(platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height);
+    }
+
+    for (player in deadPlayers){
+        fill("RED")
+        rect(deadPlayers[player].x, deadPlayers[player].y, 40, 20);
+    }
+
+    if (bullets != []) {
+        for (var i = 0; i < bullets.length; i++) {
+            if (bullets[i].shooter == socket.id){
+                fill("BLUE");
+            } else{
+                fill("RED")
+            }
+            rect(bullets[i].x, bullets[i].y, 10, 10);
+        }
+    }
+
+    if (keyIsDown(83)){
+        key = "down";
+        socket.emit('key',key);
+    } 
+    if (keyIsDown(65)){
+        key = "left";
+        socket.emit('key',key);
+    } else if (keyIsDown(68)){
+        key = "right";
+        socket.emit('key',key);
+    } else if (keyIsDown(83)){
+        key = "down";
+        socket.emit('key',key);
+    } else {
+        key = "none";
+        socket.emit('key',key);
+    }
+    if(gameStart){
+        for(player in players) {
+            if(socket.id == player){
+                fill(87, 109, 255);
+            } else {
+                fill("red");
+            }
+            rect(players[player].x, players[player].y, 20, 40);
+            fill("white")
+            rect(players[player].x - 10, players[player].y - 10, 40, 5);
+            fill("green")
+            rect(players[player].x - 10, players[player].y - 10, 40 * players[player].hp/100, 5);
+            textAlign(CENTER);
+            fill("black");
+            text(players[player].username, players[player].x + 10, players[player].y - 15);
+        }
+    }
+}
+
+function keyPressed(){
+    if (keyCode == 32) {
+        console.log('space was pressed')
+        socket.emit('shoot', 1);
+    }
+    if (keyIsDown(87)){
+        key = "up";
+        socket.emit('key',key);
+    }
+}
+
+function test(data){
+	players = data;
+    gameStart = true;
+    
+}
+
+function update(returnList){
+    bullets = returnList[0];
+    players = returnList[1];
+    platforms = returnList[2];
+    deadPlayers = returnList[3];
+}
+
+function respawn(){
+    $('#myModal').appendTo('body').modal('show');
+}
---
+++ server.js
@@ -0,0 +1,233 @@
+
+//importing the express module
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 3000);
+app.use(express.static('public'));
+
+console.log("server is running");
+
+var socket = require('socket.io');
+var io = socket(server); //keep track of inputs and outputs to the server
+io.sockets.on('connection', newConnection);
+
+//dictionary containing players for each connection
+var players = {}; // This is a dictionary where the keys are the socket ids and the value is a player object
+var bullets = [];
+var platforms = [];
+var deadPlayers = [];
+//let startTime = second();
+
+platforms.push(new Platform(0, 500, 300, 20, 0));
+platforms.push(new Platform(900, 500, 300, 20, 0));
+platforms.push(new Platform(300, 370, 200, 20, 0));
+platforms.push(new Platform(550, 350, 100, 20, 3));
+platforms.push(new Platform(700, 370, 200, 20, 0));
+platforms.push(new Platform(200, 200, 350, 20, 0));
+platforms.push(new Platform(650, 200, 350, 20, 0));
+platforms.push(new Platform(0, 310, 100, 20, 0));
+platforms.push(new Platform(1100, 310, 100, 20, 0));
+
+function newConnection(socket) {
+    console.log('new connection: ' + socket.id);
+	socket.on('disconnect', () => {	
+		console.log('lost connection: ' + socket.id);
+		delete(players[socket.id]);
+  	});
+	
+	socket.on('key', keyMsg);
+	socket.on('update', update);
+	socket.on('shoot', bulletTravel);
+	socket.on('username', processUsername);
+
+	function keyMsg(key){ //data is the key pressed
+		if(Object.keys(players).indexOf(socket.id) != -1){
+			players[socket.id].move(key);
+			io.sockets.emit('players',players);
+		// socket.broadcast.emit('key', data); // this sends to everyone minus the client that sent the message
+		}
+	}
+
+	function bulletTravel(){ 
+		//console.log(players);
+		//console.log(socket.id);
+		if (players[player].canShoot){
+			b = new Bullet(players[socket.id].x + 5, players[socket.id].y + 15, players[socket.id].dir, socket.id);
+			bullets.push(b);
+			//players[player].canShoot = false;
+		}
+	}
+	
+	function processUsername(username){
+		if(username == "") {
+			players[socket.id] = new Player(socket.id);
+		} else {
+			players[socket.id] = new Player(username);
+		}
+		io.sockets.emit('players',players);
+		gameStart = true;
+	}
+
+	function update(){
+		// update player position
+		//console.log(deadPlayers);
+		for (player in players){
+			players[player].ySpeed -= 1;
+			players[player].y -= players[player].ySpeed;
+			players[player].x -= players[player].xSpeed;
+
+			if (players[player].x > 1180){
+				players[player].x = 1180;
+			} else if (players[player].x < 0){
+				players[player].x = 0;
+			}
+			if (players[player].y < 0){
+				players[player].y = 0
+			} else if (players[player].y > 560){
+				players[player].y = 560;
+				players[player].jump = true;
+				players[player].secondJump = true;
+			}
+		}
+		// dead player physics
+		for (player in deadPlayers){
+			deadPlayers[player].ySpeed -= 1;
+			deadPlayers[player].y -= deadPlayers[player].ySpeed;
+			deadPlayers[player].x -= deadPlayers[player].xSpeed;
+
+			if (deadPlayers[player].x > 980){
+				deadPlayers[player].x = 960;
+			} else if (deadPlayers[player].x < 0){
+				deadPlayers[player].x = 0;
+			}
+			if (deadPlayers[player].y < 0){
+				deadPlayers[player].y = 0
+			} else if (deadPlayers[player].y > 560){
+				deadPlayers[player].y = 580;
+				deadPlayers[player].jump = true;
+			}
+		}
+
+		// platform collision and moving
+		for (var i = 0; i < platforms.length; i++) {
+			platforms[i].y += platforms[i].speed;
+			if (platforms[i].y > 600 - platforms[i].height || platforms[i].y < 40) {
+				platforms[i].speed = -platforms[i].speed;
+			}
+			for (player in players){
+				if (players[player].x + 20 > platforms[i].x && players[player].x < platforms[i].x + platforms[i].width && players[player].y + 40 > platforms[i].y && players[player].y < platforms[i].y + platforms[i].height) {
+					players[player].y = platforms[i].y - 40
+					players[player].ySpeed = 0;
+					players[player].jump = true;
+					players[player].secondJump = true;
+				}
+			}
+			for (player in deadPlayers){
+				if (deadPlayers[player].x + 20 > platforms[i].x && deadPlayers[player].x < platforms[i].x + platforms[i].width && deadPlayers[player].y + 40 > platforms[i].y && deadPlayers[player].y < platforms[i].y + platforms[i].height) {
+					deadPlayers[player].y = platforms[i].y - 20
+					deadPlayers[player].ySpeed = 0;
+					deadPlayers[player].jump = true;
+				}
+			}
+		}
+
+
+		var bulletsToRemove = [];
+		for (var i = 0; i < bullets.length; i++) {
+			bullets[i].move();
+			if (checkRemove(bullets[i])){
+				bulletsToRemove.push(i);
+			}
+			// check hit
+			for (player in players){
+				if (bullets[i].x > players[player].x && bullets[i].x < players[player].x + 20 && bullets[i].y > players[player].y && bullets[i].y < players[player].y + 40 && player != bullets[i].shooter){
+					players[player].hp -= 20;
+					bulletsToRemove.push(i);
+					if (players[player].hp < 0){
+						players[player].hp = 0;
+					}
+				}
+				if (players[player].hp == 0) {
+					deadPlayers.push(players[player]);
+					delete players[player];
+					io.to(player).emit("dead", 1);
+				}
+			}
+		}
+		for (i in bulletsToRemove){
+			bullets.splice(i, 1);
+		}
+
+		io.sockets.emit('returnUpdate', [bullets, players, platforms, deadPlayers]);
+	}
+}
+function Bullet(x, y, dir, shooter) {
+	this.shooter = shooter;
+	this.x = x;
+	this.y = y;
+	this.speed = 15;
+	this.dir = dir;
+	this.move = function(){
+		if(this.dir == "up"){
+			this.y -= this.speed;
+		} else if (this.dir == "down") {	
+			this.y += this.speed;
+		} else if (this.dir == "left") {
+			this.x -= this.speed;
+		} else if (this.dir == "right") {
+			this.x += this.speed;
+		}
+	}
+}
+
+function checkRemove(bullet){
+	if (bullet.x > 1200 || bullet.x < 0 || bullet.y > 600 || bullet.y < 0){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+function Player(username){
+	this.x = Math.floor(Math.random() * Math.floor(1000));;
+	this.y = Math.floor(Math.random() * Math.floor(600));;
+	this.dir = "up";
+	this.ySpeed = 0;
+	this.xSpeed = 0;
+	this.hp = 100;
+	this.jump = true;
+	this.secondJump = true;
+	this.username = username;
+	this.canShoot = true;
+
+	this.move = function(dir){
+		if(dir == "up" && this.jump == true){
+			this.jump = false;
+			this.ySpeed = 15;
+			this.dir = "up";
+		} else if(dir == "up" && this.secondJump == true){
+			this.secondJump = false;
+			this.ySpeed = 15;
+			this.dir = "up";
+		}
+		if (dir == "down") {
+			this.dir = "down";
+		} else if (dir == "left") {
+			this.xSpeed = 8;
+			this.dir = "left";
+		} else if (dir == "right") {
+			this.xSpeed = -8;
+			this.dir = "right";
+		} else {
+			this.xSpeed = 0;
+		}
+	}
+}
+
+function Platform(x, y, width, height, speed) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.speed = speed;
+}
