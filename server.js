98da556125b63de4435d879c0a13f8d8d9b3308f
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2020-12-31 17:45:39 -0500

    gravity agaiiinnnn

--- server.js
+++ server.js
@@ -210,11 +210,11 @@ function Player(username){
 	this.move = function(dir){
 		if(dir == "up" && this.jump == true){
 			this.jump = false;
-			this.ySpeed = 12;
+			this.ySpeed = 10;
 			this.dir = "up";
 		} else if(dir == "up" && this.secondJump == true){
 			this.secondJump = false;
-			this.ySpeed = 12;
+			this.ySpeed = 10;
 			this.dir = "up";
 		}
 		if (dir == "down") {
