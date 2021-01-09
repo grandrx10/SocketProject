7702ed9fe336ee25e0bbebd1adcd638d4b8e0957
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2021-01-08 21:45:53 -0500

    doco king

--- server.js
+++ server.js
@@ -626,9 +626,9 @@ function newConnection(socket) {
 							}
 						} else{
 							players[bullets[i].shooter].kills += 1;
-							if (players[bullets[i].shooter].team == 1){
+							if (players[bullets[i].shooter].team == "teamA"){
 								team1Kills += 1;
-							} else if (players[bullets[i].shooter].team == 2){
+							} else if (players[bullets[i].shooter].team == "teamB"){
 								team2Kills += 1;
 							}
 						}
